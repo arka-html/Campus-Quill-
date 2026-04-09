85e7c6df5faed2020536116c0ddb402c96e10e52
Arka Samaddar <arkasamaddar.mym@gmail.com>
2026-04-09 11:36:00 +0530

    Update script.js

--- script.js
+++ script.js
@@ -80,7 +80,7 @@ Even mind falls in acute sickness!!`
     gmail: "shreyasisingharoy1@gmail.com",
     photo: "shreyasi.jpg",
     language: "bengali",
-    bio: "শব্দের অপেক্ষায় আছি।",
+    bio: null,
     joinedDate: "April 2026",
     works: []
   },
